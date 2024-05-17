db53223101afb8c366e30788d658688db56d808f
savvyh <7277@holbertonstudents.com>
2024-05-17 08:15:32 +0000

    Fix the semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs');
 
-function FileContent(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(data);
-    });
+function FileContent (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
 }
 
 const filePath = process.argv[2];
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
 const filepath = process.argv[2];
 const str = process.argv[3];
 
 fs.writeFile(filepath, str, 'utf-8', function (err) {
-        if (err) {
-            console.log(err);
-        }
+  if (err) {
+    console.log(err);
+  }
 });
