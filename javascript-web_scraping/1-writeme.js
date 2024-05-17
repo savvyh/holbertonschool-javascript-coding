70d02457dc7ee0c4832eb55d5181ce25e58c5784
savvyh <7277@holbertonstudents.com>
2024-05-17 08:06:52 +0000

    Write a script that write a content in a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const filepath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filepath, str, 'utf-8', (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log(str);
+        }
+    });
