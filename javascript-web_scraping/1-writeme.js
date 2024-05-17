5290ceb474ac7c4709a4f58cad7c5e606c0f34dc
savvyh <7277@holbertonstudents.com>
2024-05-17 08:11:45 +0000

    Adding an error function

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const filepath = process.argv[2];
 const str = process.argv[3];
 
-fs.writeFile(filepath, str, 'utf-8', (err) => {
+fs.writeFile(filepath, str, 'utf-8', function (err) {
         if (err) {
             console.log(err);
             return;
