59e8cfb14b7b403ba85e8d46dfc4751f2942ffa4
savvyh <7277@holbertonstudents.com>
2024-05-17 09:22:19 +0000

    Fix semistandard

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,10 +5,10 @@ const movieID = process.argv[2];
 const apiSW = `https://swapi-api.hbtn.io/api/films/${movieID}`;
 
 request(apiSW, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        body = JSON.parse(body);
-        console.log(body.title);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    body = JSON.parse(body);
+    console.log(body.title);
+  }
 });
