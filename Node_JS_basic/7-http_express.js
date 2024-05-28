09d6210e4b3f1f3641d2263b0d4e4489fd71292e
savvyh <7277@holbertonstudents.com>
2024-05-28 21:15:16 +0000

    Recreate the small HTTP server using Express, b ut more complex

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const pathToFile = process.argv[2];
+  countStudents(pathToFile)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch(() => {
+      res.send('Cannot load the database');
+    });
+});
+
+app.listen(port);
+
+module.exports = app;
