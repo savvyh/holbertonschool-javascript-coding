ed4149c853b54c7af63f2192a5a655c723f0e6f4
savvyh <7277@holbertonstudents.com>
2024-05-31 09:06:21 +0000

    update the code for the task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
